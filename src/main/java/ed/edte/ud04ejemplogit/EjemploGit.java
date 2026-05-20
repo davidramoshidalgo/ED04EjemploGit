3eb4d2f0f6b5ef64102c4bbba643712beb39aa9b
davidramoshidalgo <dramos@birt.eus>
2026-05-20 16:25:53 +0200

    Version 2 cambios desde github

--- src/main/java/ed/edte/ud04ejemplogit/EjemploGit.java
+++ src/main/java/ed/edte/ud04ejemplogit/EjemploGit.java
@@ -4,11 +4,12 @@ package ed.edte.ud04ejemplogit;
 
 /**
  * Descripcion de la clase
- * @author dramo
+ * 
  */
 public class EjemploGit {
 
     public static void main(String[] args) {
         System.out.println("Ejemplo de Git");
+        System.out.println("Ejemplo de Github");
     }
 }
