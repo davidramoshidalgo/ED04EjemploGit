145fc9be95aa852055a8c05a21b549a593748732
David <dramos@birt.eus>
2026-05-20 12:27:46 +0200

    Version 0. Primer commit del proyecto

---
+++ src/main/java/ed/edte/ud04ejemplogit/EjemploGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.edte.ud04ejemplogit;
+
+/**
+ *
+ * @author dramo
+ */
+public class EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
