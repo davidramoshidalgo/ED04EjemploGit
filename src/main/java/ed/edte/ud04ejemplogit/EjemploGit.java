a2fba218318f2ab2da40e75fc4d7558f62d260e8
David <dramos@birt.eus>
2026-05-20 12:30:10 +0200

    Version 1. Commit cambios basicos

--- src/main/java/ed/edte/ud04ejemplogit/EjemploGit.java
+++ src/main/java/ed/edte/ud04ejemplogit/EjemploGit.java
@@ -1,16 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package ed.edte.ud04ejemplogit;
 
 /**
- *
+ * Descripcion de la clase
  * @author dramo
  */
 public class EjemploGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ejemplo de Git");
     }
 }
